718c1ac7ed2678b366301d305335ea71188123ab
Lea Tinoso <lxthym@icloud.com>
2025-06-26 15:07:11 +0800

    feat: add navbar, favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from './components/Hero'
 import Experience from './components/Experience'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className='bg-gray-50 flex justify-center'>
       <div className='bg-white w-[728px] h-screen shadow-sm'>
+        <Navbar />
         <Hero />
         <Experience />
       </div>

a6643f27560e3ce2b3241451655d030369ace975
Lea Tinoso <lxthym@icloud.com>
2025-07-02 21:50:58 +0800

    feat: install framer-motion, add animation on CTA

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,29 @@
+import { motion } from 'framer-motion'
+
 function Button({ text, link }) {
   if (link) {
     return (
-      <a 
+      <motion.a 
         href={link}
         className="inline-block bg-gray-800 text-gray-200 cursor-pointer hover:opacity-75 transition-opacity duration-200 px-4 py-2 rounded-full text-sm no-underline"
+        whileHover={{ scale: 1.10 }}
+        whileTap={{ scale: 0.95 }}
+        transition={{ type: "spring", stiffness: 400, damping: 10 }}
       >
         {text}
-      </a>
+      </motion.a>
     )
   }
 
   return (
-    <button 
+    <motion.button 
       className="bg-gray-800 text-gray-200 cursor-pointer hover:opacity-75 transition-opacity duration-200 px-4 py-2 rounded-full text-sm"
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
     >
       {text}
-    </button>
+    </motion.button>
   )
 }
 

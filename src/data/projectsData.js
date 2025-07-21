1020b2d43784a3bd46d338684ce17baa51871645
Lea Tinoso <lxthym@icloud.com>
2025-07-21 12:46:47 +0800

    feat: add travvy

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -5,27 +5,27 @@ export const projectsData = [
     name: "Event Page Clone",
     githubLink: "https://github.com/lea927/third-league-clone",
     deploymentLink: "https://third-league-clone.vercel.app/",
-    screenshot: "/third-league-clone.jpeg"
+    screenshot: "/third-league-clone.jpeg",
   },
   {
     id: 2,
     name: "Product Page Clone",
     githubLink: "https://github.com/lea927/willie",
     deploymentLink: "https://willie.vercel.app/",
-    screenshot: "/product-page-clone.jpeg"
+    screenshot: "/product-page-clone.jpeg",
   },
   {
     id: 3,
-    name: "Blog App",
-    githubLink: "https://github.com/lea927/dev-blog",
-    deploymentLink: "https://dev-blog-nine-xi.vercel.app/",
-    screenshot: "/blog.jpeg"
+    name: "Travvy AI",
+    githubLink: "https://github.com/lea927/travvy-ai",
+    deploymentLink: "https://travvy-c0p8i3w9k-lea-tinosos-projects.vercel.app/",
+    screenshot: "/travvy.png",
   },
   {
     id: 4,
     name: "Portfolio Website",
     githubLink: "https://github.com/lea927/portfolio-v2",
     deploymentLink: "https://portfolio-v2-sigma-puce.vercel.app/",
-    screenshot: "/portfolio.jpeg"
-  }
-]
+    screenshot: "/portfolio.jpeg",
+  },
+];
